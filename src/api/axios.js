212d2667aa9eef663052d1ddbba8f59507ad8648
estrengermithila <mithilafarjanam20@gmail.com>
2026-09-01 01:10:17 +0600

    Update production API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-baseURL: "http://localhost:5000/api"
+  baseURL: "https://nafis-imtiaj-hossain-server-opal.vercel.app/api",
 });
 
 export default axiosPublic;
